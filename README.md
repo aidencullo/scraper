396a3b44f948a6437a886f0d16dabde1b21102f7
Aiden Cullo <aidencullo@gmail.com>
2023-04-25 15:00:18 -0500

    scraping and searching for specific elements

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # scraper
+using https://realpython.com/beautiful-soup-web-scraper-python/
--- dentists/scrape.py
+++ dentists/scrape.py
@@ -1,6 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://www.opencare.com/dentists/chicago-il/"
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="ResultsContainer")
+
+print(results.prettify())
