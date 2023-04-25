f6cfe4d64ad7e13ca20f613f4b0ef4070ec9fdc9
Aiden Cullo <aidencullo@gmail.com>
2023-04-25 18:49:09 -0500

    extract links with class 'name'

--- dentists/scrape.py
+++ dentists/scrape.py
@@ -2,10 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://www.opencare.com/dentists/chicago-il/"
+
+def printResults(results_param):
+    if results is None:
+        print("no results found")
+    else:
+        for job_element in results:
+            name = job_element.text
+            print(name, end="\n" * 1)
+
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id="ResultsContainer")
+results = soup.find_all(class_="name")
 
-print(results.prettify())
+printResults(results)
+
+print("Scraping url : %s" % URL)
