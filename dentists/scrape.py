517ec69d17d75221b770e63ab1cd1521a40fb90e
Aiden Cullo <aidencullo@gmail.com>
2023-04-25 19:48:08 -0500

    add light comments

--- dentists/scrape.py
+++ dentists/scrape.py
@@ -1,16 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+"""
+Script for scraping company names and star ratings from https://www.opencare.com/dentists/chicago-il/
+"""
 
 URL = "https://www.opencare.com/dentists/chicago-il/"
 
 companies = []
 ratings = []
 
+# text manipulation
+
 def unparen(val):
     return val.replace('(','').replace(')','')  
 
 def uncomma(val):
     return val.split(',')[0]
+
+# extracing info we want from html elements
 
 def process_ratings(data):
     global ratings
@@ -30,6 +39,8 @@ def processResults(results_param, values):
             value = job_element.text
             values.append(value)
 
+# main scraper
+            
 def scrape():
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -37,11 +48,13 @@ def scrape():
     ratings_raw = soup.find_all(class_="ml-5 text-muted")
     process_ratings(ratings_raw)
     process_companies(companies_raw)
-
+    
 def print_results():
     print(list(ratings))
     print(list(companies))
 
+# file manipulation
+    
 def write_to_file():
     f = open("dentists.csv", "a")
     
@@ -55,6 +68,12 @@ def read_file():
     f = open("dentists.csv", "r")
     print(f.read())
 
+def delete_file():
+    os.remove("dentists.csv")
+
+# main
+    
+delete_file()
 scrape()
 write_to_file()
 read_file()
