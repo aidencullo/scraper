627793914d01faced1309d2ba509bc461f1f267c
Aiden Cullo <aidencullo@gmail.com>
2023-04-25 19:39:50 -0500

    extract company names and ratings and saves to csv file

--- dentists/scrape.py
+++ dentists/scrape.py
@@ -6,17 +6,21 @@ URL = "https://www.opencare.com/dentists/chicago-il/"
 companies = []
 ratings = []
 
-
 def unparen(val):
     return val.replace('(','').replace(')','')  
 
-def processRatings(data):
+def uncomma(val):
+    return val.split(',')[0]
+
+def process_ratings(data):
     global ratings
     processResults(data, ratings)
-    ratings = map(unparen, ratings)
+    ratings = list(map(unparen, ratings))
 
-def processCompanies(data):
+def process_companies(data):
+    global companies
     processResults(data, companies)
+    companies = list(map(uncomma, companies))
 
 def processResults(results_param, values):
     if results_param is None:
@@ -31,14 +35,28 @@ def scrape():
     soup = BeautifulSoup(page.content, "html.parser")
     companies_raw = soup.find_all(class_="name")
     ratings_raw = soup.find_all(class_="ml-5 text-muted")
-    processRatings(ratings_raw)
-    processCompanies(companies_raw)
+    process_ratings(ratings_raw)
+    process_companies(companies_raw)
 
-def printResults():
+def print_results():
     print(list(ratings))
     print(list(companies))
 
+def write_to_file():
+    f = open("dentists.csv", "a")
+    
+    for company, rating in zip(companies, ratings):
+        formatted_txt = "%s,%s\n" % (company, rating)
+        f.write(formatted_txt)
+
+    f.close()
+
+def read_file():
+    f = open("dentists.csv", "r")
+    print(f.read())
+
 scrape()
-printResults()
+write_to_file()
+read_file()
 
 print("Scraping url : %s" % URL)
